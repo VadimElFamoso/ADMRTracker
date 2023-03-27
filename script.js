8577fe1a66bfccb715f4bc135d7d96ff0743eddd
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2023-03-27 17:09:36 +0200

    Commit du 27.03.23
    Tentative de patch sans succès : problème au niveau de num_dep_converter();

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ let websites = [];
 let num_dep_min = 1;
 let num_dep_max = 95;
 
+//Applies conventional numerotation for french departments : 
 function num_dep_converter(num, end) {
     if(num < end){
+        let num_dep_min = 1;
         let num_str = num.toString();
         let result = "0" + num_str;
         return result;
@@ -14,30 +16,28 @@ function num_dep_converter(num, end) {
     }
 }
 
+//Builds an url for each departemental federation :
 function admrUrlBuilder(dep_num){
-    let url = "https://www.fede" + dep_num + ".admr.org";
-    websites.push(url);
-    console.log(websites);
-    return url;
+    if(dep_num < 10){
+        let url = "https://www.fede" + num_dep_converter(1, 10) + ".admr.org";
+        websites.push(url);
+        console.log(url);
+    }
+    else{
+        let url = "https://www.fede" + dep_num + ".admr.org";
+        websites.push(url);
+        console.log(url);
+        return url;    
+    }
+}
+
+for(num_dep_min ; num_dep_min <= num_dep_max ; num_dep_min++){
+    admrUrlBuilder(num_dep_min);
 }
 
 
+//Requête :
 
-websites.forEach(element => {
-    let request = new XMLHttpRequest();
-    for(let i = num_dep_min ; i<=num_dep_max ; i++){
-        request.open("GET", admrUrlBuilder(num_dep_converter(i, 10)), true);
-
-        console.log("test");
-        //On vérifie si la page a bien chargé : 
-        if((request.readyState === 4) && (request.status = 0)){
-            console.log("Requête réussie");
-        }
-        else{
-            console.log("La requête a échoué.");
-        }
-    }
-});
     
 
 
